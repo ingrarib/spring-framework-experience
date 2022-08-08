cdb224f4ebab1389ebd1977251b15446c819ad3f
ingrarib <ircm.1998@gmail.com>
2022-08-08 12:34:37 -0300

    Adicionar exercícios práticos com Spring Framework

---
+++ DESAFIO-DE-PROJETO2 padrões-de-projetos/lab-projetos-padrao-spring/src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
